d30247b7ae91c68c6f4107344c792a9b3c4370e4
Mac Mini Beta <macminibeta@Macs-Mac-mini.local>
2016-12-16 14:23:35 -0200

    Basic layout crated. PlayerViewController also created. :)

---
+++ Ratings/PlayersViewControllerTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlayersViewControllerTableViewController.h
+//  Ratings
+//
+//  Created by Mac Mini Beta on 16/12/16.
+//  Copyright © 2016 Mac Mini Beta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlayersViewControllerTableViewController : UITableViewController
+
+@end
